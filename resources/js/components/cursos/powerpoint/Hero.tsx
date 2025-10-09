a020bda25716fb17d7d6ae14939c6090e49ef34d
Enoque Peter <enoquechivoze258@gmail.com>
2025-10-09 21:53:05 +0200

    Atualizado imports de Slides

--- resources/js/components/cursos/powerpoint/Hero.tsx
+++ resources/js/components/cursos/powerpoint/Hero.tsx
@@ -1,8 +1,8 @@
 import { useEffect, useRef } from "react";
-import slide1 from "@/assets/slide 1.jpg";
-import slide2 from "@/assets/slide 2.jpg";
-import slide3 from "@/assets/slide 3.jpg";
-import slide4 from "@/assets/slide 4.jpg";
+import slide1 from "@/assets/Slide 1.jpg";
+import slide2 from "@/assets/Slide 2.jpg";
+import slide3 from "@/assets/Slide 3.jpg";
+import slide4 from "@/assets/Slide 4.jpg";
 
 const slides = [slide1, slide2, slide3, slide4, slide1, slide2, slide3, slide4];
 
